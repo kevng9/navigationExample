371b0da03227445a02bf4849a08571796f70c498
Kevin Ng <>
2018-08-18 15:35:38 -0400

    modified screen

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React, {Component}from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {createStackNavigator} from 'react-navigation';
+import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
 
 import Home from './src/Home';
 import Homework from './src/Homework';
 import Timetable from './src/Timetable';
 import ToDo from './src/ToDo';
-
+import HomeworkTab from './src/HomeworkTab';
 
 const RootNavigator = createStackNavigator({
   Home:{screen:Home},
-  Homework:{screen:Homework},
+  Homework:{screen:HomeworkTab},
   Timetable:{screen:Timetable},
   ToDo:{screen:ToDo},
 })
+
 
 export default class App extends Component{
   render(){
